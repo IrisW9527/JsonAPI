8e6959ab3baebfae403e2595226f0d10e17a7dc3
Iris Wang <irisw9527@gmail.com>
2020-02-17 11:21:47 -0500

    new

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'products/index'
   get 'products/details'
 
-  # root 'products#index'
+  root 'products#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
